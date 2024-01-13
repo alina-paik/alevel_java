21e5a696b55f10c7358c9b19da509891f9fbbf24
Аліна Паїк <alinapaik143@gmail.com>
2024-01-13 23:14:58 +0200

    add hw13 update

--- src/ua/paik/hw13/Box.java
+++ src/ua/paik/hw13/Box.java
@@ -1,24 +1,35 @@
-import com.fasterxml.jackson.annotation.JsonFormat;
-import java.time.LocalDateTime;
+package ua.paik.hw13;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class Box {
-    private String from;
-    private String material;
-    private String color;
-    private MaxLiftingCapacity maxLiftingCapacity;
-    private Cargo cargo;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime deliveryDate;
-
-
-    public static class MaxLiftingCapacity {
-        private String unit;
-        private int value;
-    }
-
-    public static class Cargo {
-        private String name;
-        private String cargoClass;
+    public static void main (String[] args) throws JsonProcessingException {
+        String json = """
+                {
+                    "from": "China",
+                    "material": "steel",
+                    "color": "black",
+                    "max-lifting-capacity": [
+                        {
+                            "unit": "kg",
+                            "value": 150
+                        }
+                    ],
+                    "cargo": [
+                        {
+                            "name": "electronics",
+                            "class": "C-1"
+                        }
+                    ],
+                    "delivery-date": "2021-09-20T11:07:00"
+                }
+                                
+                """;
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        System.out.println(objectMapper.readValue(json, BoxReader.class));
     }
 }
+
